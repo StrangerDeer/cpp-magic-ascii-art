2d2f7a185eb9cacbcb0ab327743a3453dacae722
rBabett <richterbabett03@gmail.com>
2024-01-09 13:08:54 +0100

    remove a character from the ascii characters

--- image-to-ascii/ImageToASCII.h
+++ image-to-ascii/ImageToASCII.h
@@ -12,8 +12,8 @@ public:
     ImageToASCII(const std::string &imagePath, const std::string &outputPath);
     virtual std::string getASCIIString(double scaleFactor = 1.00, bool terminalChars = false);
 protected:
-    std::string characters = " .,:;ox8%#@";
-    std::string terminalCharacters = "@#%8xo;:,. ";
+    std::string characters = " .,:;ox%#@";
+    std::string terminalCharacters = "@#%xo;:,. ";
     std::string imagePath;
     std::string outputPath;
 };
